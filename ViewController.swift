723bb989a24dd181adc760541d1260be5d814637
Chirdon Mohamed Houssein <chirdonmohamedhoussein@Chirdons-Mac.local>
2018-01-02 06:29:13 +0000

    Firts commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlterIpso
+//
+//  Created by Chirdon Mohamed Houssein on 02/01/2018.
+//  Copyright © 2018 Chirdon Mohamed Houssein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
